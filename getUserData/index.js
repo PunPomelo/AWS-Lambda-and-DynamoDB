a39f34fd18b1bbb376b54ecb09c035824aca232b
PunPomelo <pornchanok.a@pomelofashion.com>
2021-02-09 15:03:19 +0700

    use DocumentClient (dynamodb obj => std json)

--- getUserData/index.js
+++ getUserData/index.js
@@ -5,17 +5,18 @@ AWS.config.update({ region: 'us-east-2' });
 
 exports.handler = function (event, context, callback) {
   const ddb = new AWS.DynamoDB({ apiVersion: '2012-10-08' });
+  const documentClient = new AWS.DynamoDB.DocumentClient({
+    region: 'us-east-2',
+  }); // make dynamodb obj to standard json
 
   const params = {
     TableName: 'Users',
     Key: {
-      id: {
-        S: '12345', // S = String type
-      },
+      id: '12345', // S = String type
     },
   };
 
-  ddb.getItem(params, (err, data) => {
+  documentClient.get(params, (err, data) => {
     if (err) {
       console.log(err);
     }
