712890216b9b8604f37f86d32625c94fd89c2bb9
PunPomelo <pornchanok.a@pomelofashion.com>
2021-02-09 19:04:37 +0700

    refactor response function in putUserData

--- putUserData/index.js
+++ putUserData/index.js
@@ -9,19 +9,37 @@ exports.handler = async (event, context, callback) => {
     region: 'us-east-2',
   }); // make dynamodb obj to standard json
 
+  const { id, firstname, lastname } = JSON.parse(event.body);
+
   const params = {
     TableName: 'Users',
     Item: {
-      id: '67890', // S = String type
-      firstname: 'Bob',
-      lastname: 'Johnson',
+      id: id, // S = String type
+      firstname: firstname,
+      lastname: lastname,
     },
   };
 
   try {
     const data = await documentClient.put(params).promise();
-    console.log(data);
+    const responseBody = JSON.stringify(data.Item);
+    const statusCode = 201;
+
+    return response(responseBody, statusCode);
   } catch (err) {
-    console.log(err);
+    const responseBody = `Unable to put user data`;
+    const statusCode = 403;
+
+    return response(responseBody, statusCode);
   }
+};
+
+const response = (responseBody, statusCode) => {
+  return {
+    statusCode,
+    headers: {
+      myHeader: 'test',
+    },
+    body: responseBody,
+  };
 };
