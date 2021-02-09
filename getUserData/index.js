7067aa85625bf22368bfbf6cce734ce42de5e49f
PunPomelo <pornchanok.a@pomelofashion.com>
2021-02-09 14:26:33 +0700

    getUserData (getItem function)

--- getUserData/index.js
+++ getUserData/index.js
@@ -1,9 +1,24 @@
 'use strict';
 const AWS = require('aws-sdk');
 
+AWS.config.update({ region: 'us-east-2' });
+
 exports.handler = function (event, context, callback) {
-  console.log(JSON.stringify(`Event: event`));
-  // Lambda Code Here
-  // context.succeed('Success!')
-  // context.fail('Failed!')
+  const ddb = new AWS.DynamoDB({ apiVersion: '2012-10-08' });
+
+  const params = {
+    TableName: 'Users',
+    Key: {
+      id: {
+        S: '12345', // S = String type
+      },
+    },
+  };
+
+  ddb.getItem(params, (err, data) => {
+    if (err) {
+      console.log(err);
+    }
+    console.log(data);
+  });
 };
