c76dadacdb459a824409e7855b40d452ef163167
PunPomelo <pornchanok.a@pomelofashion.com>
2021-02-09 16:30:19 +0700

    use async/await

--- getUserData/index.js
+++ getUserData/index.js
@@ -3,7 +3,7 @@ const AWS = require('aws-sdk');
 
 AWS.config.update({ region: 'us-east-2' });
 
-exports.handler = function (event, context, callback) {
+exports.handler = async (event, context, callback) => {
   const ddb = new AWS.DynamoDB({ apiVersion: '2012-10-08' });
   const documentClient = new AWS.DynamoDB.DocumentClient({
     region: 'us-east-2',
@@ -16,10 +16,10 @@ exports.handler = function (event, context, callback) {
     },
   };
 
-  documentClient.get(params, (err, data) => {
-    if (err) {
-      console.log(err);
-    }
+  try {
+    const data = await documentClient.get(params).promise();
     console.log(data);
-  });
+  } catch (err) {
+    console.log(err);
+  }
 };
