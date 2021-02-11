c0bf8431425931cc64566338d99155faffe32605
PunPomelo <pornchanok.a@pomelofashion.com>
2021-02-11 18:13:54 +0700

    add item to dynamodb and clear unused variable

--- dynamodb.tf
+++ dynamodb.tf
@@ -7,7 +7,6 @@ terraform {
 }
 
 provider "aws" {
-  alias  = "us-west-1"
   region = "us-west-1"
 }
 
@@ -24,91 +23,21 @@ resource "aws_dynamodb_table" "basic-dynamodb-table" {
     type = "S"
   }
 
-#   attribute {
-#     name = "firstname"
-#     type = "S"
-#   }
-
-#   attribute {
-#     name = "lastname"
-#     type = "S"
-#   }
-
-  ttl {
-    attribute_name = "TimeToExist"
-    enabled        = false
-  }
-
-#   global_secondary_index {
-#     name               = "GameTitleIndex"
-#     hash_key           = "GameTitle"
-#     range_key          = "TopScore"
-#     write_capacity     = 10
-#     read_capacity      = 10
-#     projection_type    = "INCLUDE"
-#     non_key_attributes = ["UserId"]
-#   }
-
   tags = {
     Name        = "dynamodb-table-1"
     Environment = "production"
   }
 }
-# resource "aws_dynamodb_table" "us-east-1" {
-#   provider = aws.us-east-1
 
-#   hash_key         = "id"
-#   name             = "Users"
-#   stream_enabled   = true
-#   stream_view_type = "NEW_AND_OLD_IMAGES"
-#   read_capacity    = 1
-#   write_capacity   = 1
+resource "aws_dynamodb_table_item" "example" {
+  table_name = aws_dynamodb_table.basic-dynamodb-table.name
+  hash_key   = aws_dynamodb_table.basic-dynamodb-table.hash_key
 
-#   attribute {
-#     name = "id"
-#     type = "S"
-#   }
-#   attribute {
-#     name = "firstname"
-#     type = "S"
-#   }
-#   attribute {
-#     name = "lastname"
-#     type = "S"
-#   }
-
-# }
-
-# resource "aws_dynamodb_table" "us-west-1" {
-#   provider = aws.us-west-1
-
-#   hash_key         = "myAttribute"
-#   name             = "myTable"
-#   stream_enabled   = true
-#   stream_view_type = "NEW_AND_OLD_IMAGES"
-#   read_capacity    = 1
-#   write_capacity   = 1
-
-#   attribute {
-#     name = "myAttribute"
-#     type = "S"
-#   }
-# }
-
-# resource "aws_dynamodb_global_table" "myTable" {
-#   depends_on = [
-#     aws_dynamodb_table.us-east-1,
-#     aws_dynamodb_table.us-west-1,
-#   ]
-#   provider = aws.us-east-1
-
-#   name = "myTable"
-
-#   replica {
-#     region_name = "us-east-1"
-#   }
-
-#   replica {
-#     region_name = "us-west-1"
-#   }
-# }
+  item = <<ITEM
+{
+  "id": {"S": "12345"},
+  "firstname": {"S": "John"},
+  "lastname": {"S": "Doe"}
+}
+ITEM
+}
