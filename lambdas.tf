90db1e84ae10948caeefea5ce3cd42a63847baee
PunPomelo <78335708+PunPomelo@users.noreply.github.com>
2021-02-12 14:41:42 +0700

    add aws_iam_role_policy_attachment to terraform

--- lambdas.tf
+++ lambdas.tf
@@ -68,3 +68,8 @@ resource "aws_iam_role" "lambda_exec" {
 EOF
 
 }
+
+resource "aws_iam_role_policy_attachment" "lambda_basic_execution_role" {
+  role       = aws_iam_role.lambda_exec.name
+  policy_arn = "arn:aws:iam::aws:policy/service-role/AWSLambdaBasicExecutionRole"
+}
