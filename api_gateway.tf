9f716d9e9b16c73b0305b38728d8a013052dcf4c
PunPomelo <78335708+PunPomelo@users.noreply.github.com>
2021-02-12 19:03:18 +0700

    add aws_api_gateway_method_response UserAPIMethodPOSTResponse

--- api_gateway.tf
+++ api_gateway.tf
@@ -24,3 +24,10 @@ resource "aws_api_gateway_integration" "UserAPIIntMethodPOST" {
   type                    = "AWS_PROXY"
   uri                     = aws_lambda_function.putUserDataTF.invoke_arn
 }
+
+resource "aws_api_gateway_method_response" "UserAPIMethodPOSTResponse" {
+  rest_api_id = aws_api_gateway_rest_api.UserAPI.id
+  resource_id = aws_api_gateway_resource.UserAPIResource.id
+  http_method = aws_api_gateway_method.UserAPIMethodPOST.http_method
+  status_code = "200"
+}
