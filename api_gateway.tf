830e5746c8e020c7f09c9a18ee8de4668900f37b
PunPomelo <78335708+PunPomelo@users.noreply.github.com>
2021-02-12 18:27:39 +0700

    add aws_api_gateway_resource

--- api_gateway.tf
+++ api_gateway.tf
@@ -2,3 +2,9 @@ resource "aws_api_gateway_rest_api" "UserAPI" {
   name        = "UserAPI"
   description = "This is my API for demonstration purposes"
 }
+
+resource "aws_api_gateway_resource" "UserAPIResource" {
+  rest_api_id = aws_api_gateway_rest_api.UserAPI.id
+  parent_id   = aws_api_gateway_rest_api.UserAPI.root_resource_id
+  path_part   = "user"
+}
