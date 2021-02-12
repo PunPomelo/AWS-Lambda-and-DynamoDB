18de99521bf9dbd1d745a718f5cb8221ef72fcc5
PunPomelo <78335708+PunPomelo@users.noreply.github.com>
2021-02-12 15:26:06 +0700

    add putUserData lambda function to terraform

--- lambdas.tf
+++ lambdas.tf
@@ -25,6 +25,21 @@ resource "aws_lambda_function" "getUserDataTF" {
 
   role = aws_iam_role.lambda_exec.arn
 }
+resource "aws_lambda_function" "putUserDataTF" {
+  function_name = "putUserData"
+
+  # The bucket name as created earlier with "aws s3api create-bucket"
+  s3_bucket = "get-user-data-tf"
+  s3_key    = "v1.0.0/putUserData.zip"
+
+  # "main" is the filename within the zip file (main.js) and "handler"
+  # is the name of the property under which the handler function was
+  # exported in that file.
+  handler = "index.handler"
+  runtime = "nodejs14.x"
+
+  role = aws_iam_role.lambda_exec.arn
+}
 
 resource "aws_iam_role_policy" "dynamo_rw_access" {
   name = "DynamoReadWriteAccess"
