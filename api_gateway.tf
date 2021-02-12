83cf0926a82f3af6bcc8ae6e04f120782d69d253
PunPomelo <78335708+PunPomelo@users.noreply.github.com>
2021-02-12 18:55:25 +0700

    add aws_api_gateway_integration UserAPIMethodPOST, rename UserAPIMethod to UserAPIMethodPOST

--- api_gateway.tf
+++ api_gateway.tf
@@ -9,9 +9,18 @@ resource "aws_api_gateway_resource" "UserAPIResource" {
   path_part   = "user"
 }
 
-resource "aws_api_gateway_method" "UserAPIMethod" {
+resource "aws_api_gateway_method" "UserAPIMethodPOST" {
   rest_api_id   = aws_api_gateway_rest_api.UserAPI.id
   resource_id   = aws_api_gateway_resource.UserAPIResource.id
   http_method   = "POST"
   authorization = "NONE"
+}
+
+resource "aws_api_gateway_integration" "UserAPIIntMethodPOST" {
+  rest_api_id             = aws_api_gateway_rest_api.UserAPI.id
+  resource_id             = aws_api_gateway_resource.UserAPIResource.id
+  http_method             = aws_api_gateway_method.UserAPIMethodPOST.http_method
+  integration_http_method = "POST"
+  type                    = "AWS_PROXY"
+  uri                     = aws_lambda_function.putUserDataTF.invoke_arn
 }
