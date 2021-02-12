b68c588632a5100f19fdf1b37c4a3d2d016e9f26
PunPomelo <78335708+PunPomelo@users.noreply.github.com>
2021-02-12 18:39:12 +0700

    add aws_api_gateway_method POST method

--- api_gateway.tf
+++ api_gateway.tf
@@ -8,3 +8,10 @@ resource "aws_api_gateway_resource" "UserAPIResource" {
   parent_id   = aws_api_gateway_rest_api.UserAPI.root_resource_id
   path_part   = "user"
 }
+
+resource "aws_api_gateway_method" "UserAPIMethod" {
+  rest_api_id   = aws_api_gateway_rest_api.UserAPI.id
+  resource_id   = aws_api_gateway_resource.UserAPIResource.id
+  http_method   = "POST"
+  authorization = "NONE"
+}
