cb2227751e9e84c69087b811e275ee22ae02c270
PunPomelo <78335708+PunPomelo@users.noreply.github.com>
2021-02-18 19:05:57 +0700

    add main.tf

--- dynamodb.tf
+++ dynamodb.tf
@@ -1,22 +1,10 @@
-terraform {
-  required_providers {
-    aws = {
-      source = "hashicorp/aws"
-    }
-  }
-}
-
-provider "aws" {
-  region = "us-west-1"
-}
-
 resource "aws_dynamodb_table" "basic-dynamodb-table" {
-  name           = "Users"
-#   billing_mode   = "PROVISIONED"
+  name = "Users"
+  #   billing_mode   = "PROVISIONED"
   read_capacity  = 20
   write_capacity = 20
   hash_key       = "id"
-#   range_key      = "GameTitle"
+  #   range_key      = "GameTitle"
 
   attribute {
     name = "id"
